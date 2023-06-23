9f1e653b57d04903bec8eb5a256a35e6fb79078e
hacKarim <hmrn.karim@gmail.com>
2023-06-23 19:25:25 +0100

    Big refactor

--- src/utilities/opeai-talker/index.ts
+++ src/utilities/opeai-talker/index.ts
@@ -10,8 +10,8 @@ import ora from 'ora';
 import { userOptions as options } from 'src/utilities/constant';
 import { HermesPrompt } from 'src/utilities/opeai-talker/prompt';
 
-import { handleContinueMessage, sendMessageWithRetry } from './send-message';
 import { HermesTypeEnum, IReadFileResult } from '../types';
+import { handleContinueMessage, sendMessageWithRetry } from './send-message';
 
 export class ChatgptProxyAPI {
   private chatApi: ChatGPTUnofficialProxyAPI | ChatGPTAPI;
